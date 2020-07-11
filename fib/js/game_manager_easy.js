d89fec9c1f98b0012f8891865aab6fa86536455e
TheReal58 <68038765+TheReal58@users.noreply.github.com>
2020-07-10 20:19:26 -0700

    add fib game

--- fib/js/game_manager_easy.js
+++ fib/js/game_manager_easy.js
@@ -43,7 +43,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 1 : 2;
+    var value = Math.random() < 0.5 ? 377 : 610;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
